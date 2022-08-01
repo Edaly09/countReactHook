14b6387afa071765d810ee9cb5842c0d973c82b4
Edaly09 <edalyminthe@gmail.com>
2022-08-01 11:41:08 +0200

    Hook with counter

--- count/src/App.js
+++ count/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const App = () =>{
-const [conteur, setconteur] = useState(1)
+const [conteur, setconteur] = useState(0)
 
 const  handleCountChange1 = ()=>{
   setconteur(conteur + 1)
